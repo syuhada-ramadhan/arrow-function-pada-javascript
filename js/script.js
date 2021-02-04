c18ca40277d3c34611985b96b9d53658a7b1525b
syuhada-ramadhan <suhadahada18@gmail.com>
2021-02-04 20:30:36 +0700

    Belajar Javascript Arrow Function

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Function Expression
+const total = function (nilaiA, nilaiB) {
+  return nilaiA + nilaiB;
+};
+
+// cetak variabel total
+console.log(total(1, 3));
+
+// Arrow Function
+const hasil = (nilaiA, nilaiB) => nilaiA + nilaiB;
+
+// cetak variabel hasil
+console.log(hasil(1, 3));
